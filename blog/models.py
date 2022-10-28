adb2f4de0424ff2901b626ff8550e535c1d3a489
AASanusi <alaminm.sanusi@gmail.com>
2022-10-28 18:34:15 +0000

    Created and migrated Comment model

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
     updated_on = models.DateTimeField(auto_now=True)
     body = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -24,3 +24,17 @@ class Post(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date_added']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"

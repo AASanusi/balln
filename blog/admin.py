4ba01ea867072dbd0788a33f0ba6274caaa62a6e
AASanusi <alaminm.sanusi@gmail.com>
2022-10-28 23:05:40 +0000

    Added additional functionalities such as search and list to admin

--- blog/admin.py
+++ blog/admin.py
@@ -6,4 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'date_added')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'date_added')
     summernote_fields = ('body')
+    search_fields = ('title', 'body')

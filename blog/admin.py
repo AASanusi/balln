24287d1181ef06b6f9ddcc59fb163906ada0d6c4
AASanusi <alaminm.sanusi@gmail.com>
2022-10-28 23:22:37 +0000

    Registered comments to admin  and created additional functionalities for it

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -11,3 +11,15 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'date_added')
     summernote_fields = ('body')
     search_fields = ('title', 'body')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'date_added', 'approved')
+    list_filter = ('approved', 'date_added')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)

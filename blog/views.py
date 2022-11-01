43f83a8aa34778e34bc372545a49622fa83a08a1
AASanusi <alaminm.sanusi@gmail.com>
2022-11-01 15:09:13 +0000

    Correct views.py and post_detail and install and add allauth to requirement.txt

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,6 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": likes
+                "liked": liked
             },
         )

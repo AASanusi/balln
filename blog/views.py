c50168eeb9dc1ee64dacbaba33d647dd3e1e34c8
AASanusi <alaminm.sanusi@gmail.com>
2022-11-02 20:25:39 +0000

    Correct error in view.py for CommentForm

--- blog/views.py
+++ blog/views.py
@@ -41,7 +41,7 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        comment_form + CommentForm(data=request.Post)
+        comment_form = CommentForm(data=request.POST)
 
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email

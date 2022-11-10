ea1908967c3d96b280b8d3ea8a7ae95ec43fb0df
AASanusi <alaminm.sanusi@gmail.com>
2022-11-10 22:29:09 +0000

    Change category url

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:category_slug><slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:category_slug>/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('<slug:slug>/', views.Category.as_view(), name='category'),
     path('like/<slug:slug>', views.LikePost.as_view(), name='like_post')
 ]

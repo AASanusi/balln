38a94cc00831e83279b3a7f72200992082d0c3ec
AASanusi <alaminm.sanusi@gmail.com>
2022-11-10 19:24:11 +0000

    Update CSRF link in settings

--- balln/settings.py
+++ balln/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['balln.herokuapp.com', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['http://balln.herokuapp.com', 'https://8000-aasanusi-balln-dihemgv8e9y.ws-eu74.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['http://balln.herokuapp.com', 'https://8000-aasanusi-balln-dihemgv8e9y.ws-eu75.gitpod.io']
 
 
 # Application definition

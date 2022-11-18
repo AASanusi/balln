b68ffcbed16b014c8c667f633491fd94bcb0e770
AASanusi <alaminm.sanusi@gmail.com>
2022-11-18 09:12:57 +0000

    Edit comment and post templates, edit urls and settings

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('category/<int:id>', views.category, name='category'),
     path('like/<slug:slug>', views.LikePost.as_view(), name='like_post'),
-    path('update_comment/<slug:slug>', views.EditComment.as_view(), name='update_comment'),
-    path('delete_comment/<slug:slug>', views.DeleteComment.as_view(), name='delete_comment'),
+    path('update_comment/<int:comment_id>', views.EditComment.as_view(), name='update_comment'),
+    path('delete_comment/<int:comment_id>', views.DeleteComment.as_view(), name='delete_comment'),
 ]

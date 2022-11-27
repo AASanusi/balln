ca1f0dff4703c08ae22188fed24f5848692fc991
AASanusi <alaminm.sanusi@gmail.com>
2022-11-27 20:07:42 +0000

    Change back DEBUG to False and add XFRAMEOPTIONS and remove commented out code

--- balln/settings.py
+++ balln/settings.py
@@ -29,8 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['balln.herokuapp.com', 'localhost']
 
@@ -102,16 +103,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'balln.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
